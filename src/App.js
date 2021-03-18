84245735520ab46d26717f8e1d26d58e200e8ad5
Liam Bell <l.m.bell89@gmail.com>
2021-03-18 11:19:30 +0000

    added initial state to App

--- src/App.js
+++ src/App.js
@@ -4,30 +4,56 @@ import faker from 'faker'
 import { Api } from './api'
 
 const App = () => {
-  const [breeds, setBreeds] = useState([])
+  const [breedIds, setBreedIds] = useState([])
+  const [products, setProducts] = useState([])
+  const [basket, setBasket] = useState([])
 
   useEffect(() => {
-    Api.getBreedIds().then(arr => setBreeds(arr))
+    Api.getBreedIds().then(arr => setBreedIds(arr))
   }, [])
 
-  const getCat = async () => {
-    if (!breeds || breeds.length === 0) return
+  useEffect(() => {
+    getCats(10).then(arr => {
+      setProducts(arr)
+      console.log(arr)
+    })
+  }, [breedIds])
 
-    const breedId = breeds[Math.floor(Math.random() * breeds.length)]
-    const cat = await Api.getCatByBreed(breedId)
 
-    cat.price = Math.floor(Math.random() * 100)
-    cat.name = faker.name.findName()
-    cat.email = faker.internet.email()
-    cat.job = faker.name.jobTitle()
-    cat.gender = faker.name.gender()
+  const getCats = async (number) => {
+    if (!breedIds || breedIds.length === 0) return
 
-    return cat
+    let catArr = []
+
+    for (let i = 0; i < number; i++) {
+      const breedId = breedIds[Math.floor(Math.random() * breedIds.length)]
+      const cat = Api.getCatByBreed(breedId)  
+      catArr.push(cat)
+    }
+
+    catArr = await Promise.all(catArr)
+
+    return catArr.map(cat => {
+      cat.price = Math.floor(Math.random() * 10000) / 100 + 10
+      cat.name = faker.name.findName()
+      cat.email = faker.internet.email()
+      cat.job = faker.name.jobTitle()
+      cat.gender = faker.name.gender()
+      cat.music = faker.music.genre()
+      cat.car = faker.fake("{{vehicle.color}} {{vehicle.manufacturer}}, {{vehicle.model}}")
+      cat.image = cat[0]
+      cat.breed = cat[0].breeds[0]
+
+      delete cat[0]
+      delete cat.image.breeds
+
+      return cat
+    })
   }
 
   return (
     <div className="App">
-      <button onClick={() => getCat().then(cat => console.log(cat))}>Log a cat to console</button>
+      <button onClick={() => getCats(1).then(cat => console.log(cat[0]))}>Log a cat to console</button>
     </div>
   ) 
 }
