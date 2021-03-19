68a56601ee6bf003e2adfac59267e6f60b093142
Liam Bell <l.m.bell89@gmail.com>
2021-03-19 18:57:01 +0000

    added basename to Router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cats4lyf">
       <App />
     </BrowserRouter>    
   </React.StrictMode>,
